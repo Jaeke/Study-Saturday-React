47d114f89d3f60ef501870584472947222bb3bac
Jake Chen <jakechen825@gmail.com>
2021-03-20 12:52:57 -0400

    Finished cycle-1

---
+++ browser/components/main.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import axios from 'axios'
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    this.getStudents();
+  }
+
+  async getStudents() {
+    try {
+      const { data } = await axios.get('/students');
+      this.setState({ students:data });
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render() {
+    // const { students } = this.state;
+    return (
+      <div>
+        <h1>Students</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.students.map((student) => {
+              return (
+                <tr key={student.id}>
+                  <td>{student.fullName}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
--- browser/index.js
+++ browser/index.js
@@ -1,4 +1,11 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Main from './components/main'
 
-console.log('Hello Webpack!');
+
+ReactDOM.render(
+  <Main />, document.getElementById('app')
+)
+
